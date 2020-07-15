1f10d2de976daaf1d1e1b51f221449dcc1d4abe5
zainarbani <zaintsyariev@gmail.com>
2020-07-15 21:50:16 +0700

    userbot: weather: Add more weather provider
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>
    
    sample_config.env removed cuz we're using oldway to deploy.
    
    Signed-off-by: goodmeow <harunbam3@gmail.com>
    
    Conflicts:
    	sample_config.env
    	userbot/__init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -105,8 +105,9 @@ CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
 GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
 
 # OpenWeatherMap API Key
-OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY", None)
+OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID") or None
+WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY") or None
+WEATHER_DEFLANG = os.environ.get("WEATHER_DEFLANG") or None
 
 # Anti Spambot Config
 ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
