40e52a5b587eb30ccf8ab7954d51e9da2759ffd5
goodmeow <harunbam3@gmail.com>
2020-08-13 10:05:39 +0700

    modules: sangmata: add more function
    - allow search by id
    - clean-up message after search completed
    - add execption if bot doesn't respond

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -7,39 +7,56 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.events import register
 from userbot import bot, CMD_HELP
 
-@register(outgoing=True, pattern="^.sg(?: |$)(.*)")
-async def _(event):
+@register(outgoing=True, pattern=r"^\.samata(?: |$)(.*)")
+async def lastname(event):
     if event.fwd_from:
-        return 
+        return
     if not event.reply_to_msg_id:
-       await event.edit("`Reply to any user message.`")
-       return
-    reply_message = await event.get_reply_message() 
-    if not reply_message.text:
-       await event.edit("```reply to text message```")
-       return
+        await event.edit("```Reply to any user message.```")
+        return
+    message = await event.get_reply_message()
     chat = "@SangMataInfo_bot"
-    sender = reply_message.sender
-    if reply_message.sender.bot:
-       await event.edit("`Reply to actual users message.`")
-       return
-    await event.edit("`Processing`")
-    async with bot.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
-              await bot.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("`Please unblock @sangmatainfo_bot and try again`")
-              return
-          if response.text.startswith("Forward"):
-             await event.edit("`can you kindly disable your forward privacy settings for good?`")
-          else: 
-             await event.edit(f"{response.message.message}")
+    user_id = message.sender.id
+    id = f"/search_id {user_id}"
+    if message.sender.bot:
+        await event.edit("```Reply to actual users message.```")
+        return
+    await event.edit("```Sit tight while we crunching data from NSA```")
+    try:
+        async with bot.conversation(chat) as conv:
+            try:
+                msg = await conv.send_message(id)
+                r = await conv.get_response()
+                response = await conv.get_response()
+            except YouBlockedUserError:
+                await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+                return
+            if r.text.startswith("Name"):
+                respond = await conv.get_response()
+                await event.edit(f"{r.message}")
+                await event.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id, respond.id]
+                )
+                return
+            if response.text.startswith(
+                    "No records") or r.text.startswith("No records"):
+                await event.edit("```No records found for this user```")
+                await event.client.delete_messages(
+                    conv.chat_id, [msg.id, r.id, response.id]
+                )
+                return
+            else:
+                respond = await conv.get_response()
+                await event.edit(f"{response.message}")
+            await event.client.delete_messages(
+                conv.chat_id, [msg.id, r.id, response.id, respond.id]
+            )
+    except TimeoutError:
+        return await event.edit("`Error: `@SangMataInfo_bot` is not responding!.`")
 
 
 CMD_HELP.update({
         "sangmata": 
-        "`>.sg`"
+        "`>.samata`"
         "\nUsage: View user history."
 })
