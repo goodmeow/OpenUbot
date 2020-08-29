1615d5d32e89b2e01c8539e42150e31bfc3594f5
Angga <angga@linuxmail.org>
2020-08-29 17:39:10 +0700

    system_stats: alive: add bot uptime & cleanup
    Change-Id: I52e8abcb0b7e858ed4e5bef27837ca2110e2021b
    
    Signed-off-by: Angga <angga@linuxmail.org>
    Signed-off-by: MoveAngel <moveangel@minaproject.tech>
    Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>
    Signed-off-by: goodmeow <harunbam3@gmail.com>
    
    Conflicts:
    	userbot/__init__.py
    	userbot/modules/system_stats.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -6,7 +6,8 @@
 """ Userbot initialization. """
 
 import os
-
+import re
+import time
 from sys import version_info
 from logging import basicConfig, getLogger, INFO, DEBUG
 from distutils.util import strtobool as sb
@@ -19,6 +20,8 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 load_dotenv("config.env")
+
+StartTime = time.time()
 
 # Bot Logs setup:
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -4,21 +4,50 @@
 # you may not use this file except in compliance with the License.
 #
 """ Userbot module for getting information about the server. """
-
+import platform
+import sys
+import time
 from asyncio import create_subprocess_shell as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from platform import python_version, uname
 from shutil import which
 from os import remove
-from telethon import version
 
-from userbot import CMD_HELP, ALIVE_NAME
+from userbot import CMD_HELP, ALIVE_NAME, StartTime
+from telethon import __version__, version
 from userbot.events import register
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
 
 @register(outgoing=True, pattern="^.sysd$")
 async def sysdetails(sysd):
@@ -126,7 +155,8 @@ async def amireallyalive(alive):
                      "Userbot Service is Running!! \n\n"
                      f"Telethon Ver.: {version.__version__} \n"
                      f"Python   Ver.: {python_version()} \n"
-                     f"User         : {DEFAULTUSER}"
+                     f"User         : {DEFAULTUSER} \n"
+                     f"Uptime       : {uptime} \n"
                      "`")
 
 
